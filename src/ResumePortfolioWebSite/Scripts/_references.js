97c937cbb23bdda8cc377732d421ff0de87268e8
Igor Fesenko <igor.aka.ky7m@gmail.com>
2015-01-25 00:25:01 +0200

    Initial commit with "Coming soon" stub

---
+++ src/ResumePortfolioWebSite/Scripts/_references.js
@@ -0,0 +1,7 @@
+﻿/// <autosync enabled="true" />
+/// <reference path="respond.js" />
+/// <reference path="jquery.validate.unobtrusive.js" />
+/// <reference path="npm.js" />
+/// <reference path="bootstrap.min.js" />
+/// <reference path="jquery-2.1.3.js" />
+/// <reference path="jquery.validate.js" />
