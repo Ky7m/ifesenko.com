671d2635d42bd3a956a18f1ef8144a8f15035238
Igor Fesenko <igor.aka.ky7m@gmail.com>
2015-01-25 03:10:01 +0200

    Added Google Analytics

---
+++ src/ResumePortfolioWebSite/Extensions/AnalyticsHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using System.Web.Mvc;
+
+namespace ResumePortfolioWebSite.Extensions
+{
+	public static class AnalyticsHelper
+	{
+		public static MvcHtmlString GetAnalyticsScript()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.Append("<script>(function(G,o,O,g,l){G.GoogleAnalyticsObject=O;G[O]||(G[O]=function()");
+			sb.Append("{(G[O].q=G[O].q||[]).push(arguments)});G[O].l=+new ");
+			sb.Append("Date;g=o.createElement('script'),l=o.scripts[0];g.src='//www.google-");
+			sb.Append("analytics.com/analytics.js';l.parentNode.insertBefore(g,l)}");
+			sb.Append("(this,document,'ga'));ga('create','UA-58923658-1', 'auto');ga('send','pageview')</script>");
+			return MvcHtmlString.Create(sb.ToString());
+		}
+	}
+}
