64d0ee0242e9927df455e7e228799e5bfd93a837
Igor Fesenko <ifesen@softserveinc.com>
2015-01-27 18:25:06 +0200

    Updated jpg images. Changed sctructure of css files.

--- src/ResumePortfolioWebSite/App_Start/BundleConfig.cs
+++ src/ResumePortfolioWebSite/App_Start/BundleConfig.cs
@@ -25,10 +25,11 @@ namespace ResumePortfolioWebSite
 
             bundles.Add(new ScriptBundle("~/bundles/app").Include("~/Scripts/app/*.js"));
 
-            bundles.Add(new StyleBundle("~/Content/sitecss").Include(
+            bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/animate.css",
-                      "~/Content/css/font-awesome.css",
+                      "~/Content/font-awesome.css",
+                      "~/Content/jquery.smooth.scroll.css",
                       "~/Content/OwlCarousel/owl.carousel.css",
                       "~/Content/OwlCarousel/owl.theme.css",
                       "~/Content/OwlCarousel/owl.transitions.css",
