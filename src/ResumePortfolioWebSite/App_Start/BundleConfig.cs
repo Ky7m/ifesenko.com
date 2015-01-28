80d94921f7018fbdf3bbe40573c5657504507821
Igor Fesenko <ifesen@softserveinc.com>
2015-01-28 20:14:19 +0200

    Moved jquery, bootstrap, respond and fontawesome to CDNs.

--- src/ResumePortfolioWebSite/App_Start/BundleConfig.cs
+++ src/ResumePortfolioWebSite/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Optimization;
+using ResumePortfolioWebSite.Extensions;
 
 namespace ResumePortfolioWebSite
 {
@@ -6,13 +7,29 @@ namespace ResumePortfolioWebSite
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+            //BundleTable.EnableOptimizations = true; //force optimization while debugging
+
+            var jquery = new ScriptBundle("~/bundles/jquery",
+                "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js")
+                .Include("~/Scripts/jquery-{version}.js");
+            jquery.CdnFallbackExpression = "window.jQuery";
+            bundles.Add(jquery);
+
+            var bootstrapJs = new ScriptBundle("~/bundles/bootstrapJs",
+                "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js")
+                .Include("~/Scripts/bootstrap.min.js");
+            bootstrapJs.CdnFallbackExpression = "window.jQuery.fn.modal";
+            bundles.Add(bootstrapJs);
+
+            var respondJs = new ScriptBundle("~/bundles/respondJs",
+                "//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js")
+                .Include("~/Scripts/respond.min.js");
+            respondJs.CdnFallbackExpression = "window.respond";
+            bundles.Add(respondJs);
+
+
             bundles.Add(new ScriptBundle("~/bundles/js").Include(
-
-                "~/Scripts/jquery-{version}.js",
-
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js",
-
                 "~/Scripts/jquery.countTo.js",
                 "~/Scripts/jquery.simple-text-rotator.js",
                 "~/Scripts/jquery.waypoints.js",
@@ -23,10 +40,16 @@ namespace ResumePortfolioWebSite
 
                 "~/Scripts/app/*.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle("~/bundles/bootstrapCss",
+               "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css")
+               .IncludeFallback("~/Content/bootstrap.min.css", "sr-only", "width", "1px"));
+
+            bundles.Add(new StyleBundle("~/bundles/font-awesome",
+               "//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css")
+               .IncludeFallback("~/Content/font-awesome.min.css", "fa", "fontFamily", "FontAwesome"));
+
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                       "~/Content/animate.css",
-                      "~/Content/font-awesome.css",
                       "~/Content/jquery.smooth.scroll.css",
                       "~/Content/OwlCarousel/owl.carousel.css",
                       "~/Content/OwlCarousel/owl.theme.css",
