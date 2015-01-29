59c5f4723f008689cc898730d497a434308f8806
Igor Fesenko <ifesen@softserveinc.com>
2015-01-29 13:34:07 +0200

    Disabled some fields in response header

--- src/ResumePortfolioWebSite/Global.asax.cs
+++ src/ResumePortfolioWebSite/Global.asax.cs
@@ -6,20 +6,21 @@ using System.Web.Routing;
 
 namespace ResumePortfolioWebSite
 {
-	public class MvcApplication : HttpApplication
-	{
-		protected void Application_Start()
-		{
-			AreaRegistration.RegisterAllAreas();
-			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
-			BundleConfig.RegisterBundles(BundleTable.Bundles);
-		}
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            MvcHandler.DisableMvcResponseHeader = true;
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
 
-		protected void Application_Error(object sender, EventArgs e)
-		{
-			//handle exceptions, send them via email, whatever
-			var exception = Server.GetLastError();
-		}
-	}
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            //handle exceptions, send them via email, whatever
+            var exception = Server.GetLastError();
+        }
+    }
 }
