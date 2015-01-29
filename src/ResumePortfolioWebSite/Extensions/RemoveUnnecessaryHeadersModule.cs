ab9cfcde67044529afe3bb962cf55288582c6730
Igor Fesenko <ifesen@softserveinc.com>
2015-01-29 13:50:41 +0200

    Added http module for removing Server information from header response

---
+++ src/ResumePortfolioWebSite/Extensions/RemoveUnnecessaryHeadersModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+
+namespace ResumePortfolioWebSite.Extensions
+{
+    public class RemoveUnnecessaryHeadersModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            // This only works if running in IIS7+ Integrated Pipeline mode
+            if (!HttpRuntime.UsingIntegratedPipeline) return;
+            context.PreSendRequestHeaders += (sender, e) =>
+            {
+                var app = sender as HttpApplication;
+                if (app != null && app.Context != null)
+                {
+                    app.Context.Response.Headers.Remove("Server");
+                }
+            };
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
