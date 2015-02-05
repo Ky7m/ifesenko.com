094b2a637eadc26f000c00c477867d5dfb0b33e7
Igor Fesenko <ifesen@softserveinc.com>
2015-02-05 16:35:02 +0200

    Added Application Insights telemetry

---
+++ src/PersonalHomePage/Extensions/TelemetryBuildVersionContextInitializer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using PersonalHomePage.Controllers;
+
+namespace PersonalHomePage.Extensions
+{
+    public class TelemetryBuildVersionContextInitializer : IContextInitializer
+    {
+        public void Initialize(TelemetryContext context)
+        {
+            context.Properties["BuildVersion"] = System.Reflection.Assembly.GetAssembly(typeof(HomeController)).GetName().Version.ToString();
+        }
+    }
+}
--- src/PersonalHomePage/Global.asax.cs
+++ src/PersonalHomePage/Global.asax.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using PersonalHomePage.Extensions;
 
 namespace PersonalHomePage
 {
     public class MvcApplication : HttpApplication
     {
+        private readonly Lazy<TelemetryClient> _telemetryClient = new Lazy<TelemetryClient>(() => new TelemetryClient(), LazyThreadSafetyMode.ExecutionAndPublication);
+
         protected void Application_Start()
         {
+            TelemetryConfiguration.Active.ContextInitializers.Add(new TelemetryBuildVersionContextInitializer());
+
             MvcHandler.DisableMvcResponseHeader = true;
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -19,8 +28,20 @@ namespace PersonalHomePage
 
         protected void Application_Error(object sender, EventArgs e)
         {
-            //handle exceptions, send them via email, whatever
             var exception = Server.GetLastError();
+            WriteExceptionToDebugConsole(exception);
+            WriteExceptionToApplicationInsights(exception);
+        }
+
+        [Conditional("DEBUG")]
+        private void WriteExceptionToDebugConsole(Exception exception)
+        {
+            Debug.WriteLine(exception.ToString());
+        }
+
+        private void WriteExceptionToApplicationInsights(Exception exception)
+        {
+            _telemetryClient.Value.TrackException(exception);
         }
     }
 }
