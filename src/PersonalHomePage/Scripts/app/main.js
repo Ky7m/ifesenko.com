9fc5dd2abd02df4479baa0c94906cf36c11de844
Igor Fesenko <ifesen@softserveinc.com>
2015-02-05 17:48:42 +0200

    Code refactoring. Removed ga strong typed definition.

--- src/PersonalHomePage/Scripts/app/main.js
+++ src/PersonalHomePage/Scripts/app/main.js
@@ -1,60 +1,60 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
-var Main;
-(function (Main) {
+var MainModule;
+(function (MainModule) {
     // Preloader
     $(window).load(function () {
-        $('#status').fadeOut();
-        $('#preloader').fadeOut('slow');
+        $("#status").fadeOut();
+        $("#preloader").fadeOut("slow");
     });
-    $(document).ready(function () {
-        $('body').scrollspy({
-            target: '.navbar-custom',
+    $(function () {
+        $("body").scrollspy({
+            target: ".navbar-custom",
             offset: 50
         });
-        $(document).on('click', '.navbar-collapse.in', function (e) {
-            if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
-                $(this).collapse('hide');
+        $(document).on("click", ".navbar-collapse.in", function (e) {
+            if ($(e.target).is("a") && $(e.target).attr("class") !== "dropdown-toggle") {
+                $(this).collapse("hide");
             }
         });
-        $('a[href*=#]').bind("click", function (e) {
+        $("a[href*=#]").bind("click", function (e) {
             var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
+            $("html, body").stop().animate({
+                scrollTop: $(anchor.attr("href")).offset().top
             }, 1000);
             e.preventDefault();
         });
         // Background image
-        var $intro = $('#intro');
+        var $intro = $("#intro");
         if ($intro) {
             var imgUrl = $intro.attr("data-backstretch-image");
             $intro.backstretch([imgUrl]);
         }
         // Navbar
-        var navbar = $('.navbar');
+        var navbar = $(".navbar");
         var navHeight = navbar.height();
         $(window).scroll(function () {
             if ($(this).scrollTop() >= navHeight) {
-                navbar.addClass('navbar-color');
+                navbar.addClass("navbar-color");
             }
             else {
-                navbar.removeClass('navbar-color');
+                navbar.removeClass("navbar-color");
             }
         });
         if ($(window).width() <= 767) {
-            navbar.addClass('custom-collapse');
+            navbar.addClass("custom-collapse");
         }
         $(window).resize(function () {
             if ($(this).width() <= 767) {
-                navbar.addClass('custom-collapse');
+                navbar.addClass("custom-collapse");
             }
             else {
-                navbar.removeClass('custom-collapse');
+                navbar.removeClass("custom-collapse");
             }
         });
         // Count to
-        $('#stats').waypoint(function () {
-            $('.timer').each(function () {
-                var counter = $(this).attr('data-count');
+        $("#stats").waypoint(function () {
+            $(".timer").each(function () {
+                var counter = $(this).attr("data-count");
                 $(this).delay(6000).countTo({
                     from: 0,
                     to: counter,
@@ -62,7 +62,7 @@ var Main;
                     refreshInterval: 50
                 });
             });
-        }, { offset: '70%', triggerOnce: true });
+        }, { offset: "70%", triggerOnce: true });
         // WOW Animation When You Scroll
         var wow = new WOW({
             mobile: false
@@ -82,5 +82,5 @@ var Main;
             speed: 3000
         });
     });
-})(Main || (Main = {}));
+})(MainModule || (MainModule = {}));
 //# sourceMappingURL=main.js.map
--- src/PersonalHomePage/Scripts/app/main.ts
+++ src/PersonalHomePage/Scripts/app/main.ts
@@ -1,37 +1,37 @@
 ﻿/// <reference path="../typings/jquery/jquery.d.ts" />
 
-module Main {
+module MainModule {
     declare var WOW: any;
     declare var $: any;
     
     // Preloader
     $(window).load(() => {
-        $('#status').fadeOut();
-        $('#preloader').fadeOut('slow');
+        $("#status").fadeOut();
+        $("#preloader").fadeOut("slow");
     });
 
-    $(document).ready(() => {
-        $('body').scrollspy({
-            target: '.navbar-custom',
+    $(() => {
+        $("body").scrollspy({
+            target: ".navbar-custom",
             offset: 50
         });
 
-        $(document).on('click', '.navbar-collapse.in', function (e) {
-            if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
-                $(this).collapse('hide');
+        $(document).on("click", ".navbar-collapse.in", function (e) {
+            if ($(e.target).is("a") && $(e.target).attr("class") !== "dropdown-toggle") {
+                $(this).collapse("hide");
             }
         });
 
-        $('a[href*=#]').bind("click", function (e) {
+        $("a[href*=#]").bind("click", function (e) {
             var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
+            $("html, body").stop().animate({
+                scrollTop: $(anchor.attr("href")).offset().top
             }, 1000);
             e.preventDefault();
         });
         
         // Background image
-        var $intro = $('#intro');
+        var $intro = $("#intro");
         if ($intro) {
             var imgUrl = $intro.attr("data-backstretch-image");
             $intro.backstretch([imgUrl]);
@@ -46,35 +46,35 @@ module Main {
         } 
         
         // Navbar
-        var navbar = $('.navbar');
+        var navbar = $(".navbar");
         var navHeight = navbar.height();
 
         $(window).scroll(function () {
             if ($(this).scrollTop() >= navHeight) {
-                navbar.addClass('navbar-color');
+                navbar.addClass("navbar-color");
             }
             else {
-                navbar.removeClass('navbar-color');
+                navbar.removeClass("navbar-color");
             }
         });
 
         if ($(window).width() <= 767) {
-            navbar.addClass('custom-collapse');
+            navbar.addClass("custom-collapse");
         }
 
         $(window).resize(function () {
             if ($(this).width() <= 767) {
-                navbar.addClass('custom-collapse');
+                navbar.addClass("custom-collapse");
             }
             else {
-                navbar.removeClass('custom-collapse');
+                navbar.removeClass("custom-collapse");
             }
         });
 
         // Count to
-        $('#stats').waypoint(() => {
-            $('.timer').each(function () {
-                var counter = $(this).attr('data-count');
+        $("#stats").waypoint(() => {
+            $(".timer").each(function () {
+                var counter = $(this).attr("data-count");
                 $(this).delay(6000).countTo({
                     from: 0,
                     to: counter,
@@ -82,7 +82,7 @@ module Main {
                     refreshInterval: 50
                 });
             });
-        }, { offset: '70%', triggerOnce: true });
+        }, { offset: "70%", triggerOnce: true });
 
         // WOW Animation When You Scroll
         var wow = new WOW({
