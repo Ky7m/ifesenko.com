f51f72eb925477e3d59de1395fb99f3bb294b701
Igor Fesenko <ifesen@softserveinc.com>
2015-02-06 12:22:33 +0200

    Refactored a bit error controller

--- src/PersonalHomePage/Controllers/ErrorController.cs
+++ src/PersonalHomePage/Controllers/ErrorController.cs
@@ -1,19 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using System.Net;
+using System.Web.Mvc;
+using System.Web.UI;
+using PersonalHomePage.Extensions.IframeOptions;
+using WebMarkupMin.Mvc.ActionFilters;
 
 namespace PersonalHomePage.Controllers
 {
-  public class ErrorController : Controller
-  {
-    public ActionResult Index()
+    [NoIFrame]
+    [OutputCache(VaryByParam = "*", Duration = 6000, Location = OutputCacheLocation.Downstream)]
+    public class ErrorController : Controller
     {
-            Response.StatusCode = 500;
+        [CompressContent]
+        [MinifyHtml]
+        public ActionResult Index()
+        {
+            Response.StatusCode = (int) HttpStatusCode.InternalServerError;
             return View();
-    }
+        }
 
-    public ActionResult NotFound()
-    {
-            Response.StatusCode = 404;
+        [CompressContent]
+        [MinifyHtml]
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = (int)HttpStatusCode.NotFound;
             return View();
+        }
     }
-  }
 }
