5f115bdf1e6b8dfb1f6d254ae406c847ab2024ec
Igor Fesenko <ifesen@softserveinc.com>
2015-02-18 18:16:20 +0200

    A lot of modification related to IIS and MVC frameworks

--- src/PersonalHomePage/App_Start/RouteConfig.cs
+++ src/PersonalHomePage/App_Start/RouteConfig.cs
@@ -3,17 +3,28 @@ using System.Web.Routing;
 
 namespace PersonalHomePage
 {
-	public class RouteConfig
-	{
-		public static void RegisterRoutes(RouteCollection routes)
-		{
-			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            // Imprive SEO by stopping duplicate URL's due to case or trailing slashes.
+            routes.AppendTrailingSlash = true;
+            routes.LowercaseUrls = true;
 
-			routes.MapRoute(
-				name: "Default",
-				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-			);
-		}
-	}
+            // IgnoreRoute - Tell the routing system to ignore certain routes for better performance.
+            // Ignore .axd files.
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            // Ignore everything in the Content folder.
+            routes.IgnoreRoute("Content/{*pathInfo}");
+            // Ignore everything in the Scripts folder.
+            routes.IgnoreRoute("Scripts/{*pathInfo}");
+            // Ignore the humans.txt file.
+            routes.IgnoreRoute("humans.txt");
+
+             routes.MapRoute(
+                 name: "Default",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
 }
