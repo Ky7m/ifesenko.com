cdda86db2f6c274fae3c741b3241ca1d1b657d4e
Igor Fesenko <ifesen@softserveinc.com>
2015-02-22 16:28:34 +0200

    Added dummy service for email service

---
+++ src/PersonalHomePage/Models/EmailMessageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PersonalHomePage.Models
+{
+    public class EmailMessageModel
+    {
+        public string Destination { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ src/PersonalHomePage/Services/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using PersonalHomePage.Models;
+using SendGrid;
+
+namespace PersonalHomePage.Services
+{
+    public class EmailService
+    {
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task SendEmailAsync(EmailMessageModel message)
+        {
+            var myMessage = new SendGridMessage();
+
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new MailAddress("noreply@ifesenko.com", "No reply service");
+            myMessage.Subject = message.Subject;
+            myMessage.Text = message.Body;
+            myMessage.Html = message.Body;
+
+            var credentials = new NetworkCredential(ConfigurationManager.AppSettings["emailService:Account"],
+                                                    ConfigurationManager.AppSettings["emailService:Password"]);
+
+            // Create a Web transport for sending email.
+            var transportWeb = new Web(credentials);
+            await transportWeb.DeliverAsync(myMessage);
+
+        }
+    }
+}
