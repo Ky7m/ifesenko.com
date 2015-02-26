17816bac0d7851caafa4c7a8ca3c7ed710cd8698
Igor Fesenko <ifesen@softserveinc.com>
2015-02-26 15:59:32 +0200

    Removed custom binding handler for backstrech

--- src/PersonalHomePage/Scripts/bindingHandlers/ko.backstretch.js
+++
@@ -1,9 +0,0 @@
-/// <reference path="../typings/knockout/bindinghandlersregister.d.ts" />
-/// <reference path="../typings/jquery/jquerypluginsregister.d.ts" />
-ko.bindingHandlers.backstretch = {
-    init: function (element, valueAccessor) {
-        var value = ko.unwrap(valueAccessor());
-        $(element).backstretch(value);
-    }
-};
-//# sourceMappingURL=ko.backstretch.js.map
