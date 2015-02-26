f1d33808bdf25fa917fd07c645d29ba599efec90
Igor Fesenko <ifesen@softserveinc.com>
2015-02-26 15:50:12 +0200

    Embedded images into js and html

--- src/PersonalHomePage/Scripts/app/models/certification.js
+++ src/PersonalHomePage/Scripts/app/models/certification.js
@@ -1,28 +1,14 @@
-var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    __.prototype = b.prototype;
-    d.prototype = new __();
-};
 var PersonalHomePage;
 (function (PersonalHomePage) {
     var Models;
     (function (Models) {
-        var Certification = (function (_super) {
-            __extends(Certification, _super);
-            function Certification(relativePath, description) {
-                _super.call(this, relativePath);
+        var Certification = (function () {
+            function Certification(image, description) {
+                this.image = image;
                 this.description = description;
-                this.logoSource = this.CdnPath;
             }
-            Certification.prototype.onError = function (imageContext) {
-                if (this.LocalPath) {
-                    imageContext.onerror = null;
-                    imageContext.src = this.LocalPath;
-                }
-            };
             return Certification;
-        })(PersonalHomePage.Base.BaseContentItem);
+        })();
         Models.Certification = Certification;
     })(Models = PersonalHomePage.Models || (PersonalHomePage.Models = {}));
 })(PersonalHomePage || (PersonalHomePage = {}));
