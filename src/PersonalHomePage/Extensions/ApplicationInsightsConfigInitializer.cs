93c2993da122d3dc4555ec260fd35cdf4363a20e
Igor Fesenko <ifesen@softserveinc.com>
2015-08-27 12:40:46 +0300

    Add telemetry configuration

---
+++ src/PersonalHomePage/Extensions/ApplicationInsightsConfigInitializer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ApplicationInsights.Extensibility;
+
+namespace PersonalHomePage.Extensions
+{
+    public class ApplicationInsightsConfigInitializer
+         : IContextInitializer
+    {
+
+        public void Initialize(Microsoft.ApplicationInsights.DataContracts.TelemetryContext context)
+        {
+            // Configure app version
+            context.Component.Version = BundleConfig.Version;
+        }
+    }
+}
