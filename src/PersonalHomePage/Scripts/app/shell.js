1019874ab64a70bf743a99a4e5e55e734bd7245d
Igor Fesenko <igor.aka.ky7m@gmail.com>
2016-01-10 21:19:50 +0200

    Fix selector issue

--- src/PersonalHomePage/Scripts/app/shell.js
+++ src/PersonalHomePage/Scripts/app/shell.js
@@ -12,7 +12,7 @@ var PersonalHomePage;
                     $(this).collapse("hide");
                 }
             });
-            $("a[href*=#]").bind("click", function (e) {
+            $("a[href*='#']").bind("click", function (e) {
                 var anchor = $(this);
                 $("html, body").stop().animate({
                     scrollTop: $(anchor.attr("href")).offset().top
--- src/PersonalHomePage/Scripts/app/shell.ts
+++ src/PersonalHomePage/Scripts/app/shell.ts
@@ -12,7 +12,7 @@
             }
         });
 
-        $("a[href*=#]").bind("click", function (e) {
+        $("a[href*='#']").bind("click", function (e) {
             var anchor = $(this);
             $("html, body").stop().animate({
                 scrollTop: $(anchor.attr("href")).offset().top
