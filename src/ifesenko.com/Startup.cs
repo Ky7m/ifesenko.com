a41a18ad12b67bb864120979f03ee78d1b6966c0
Igor Fesenko <ifesen@softserveinc.com>
2016-03-14 19:22:03 +0200

    Fixes settings

--- src/ifesenko.com/Startup.cs
+++ src/ifesenko.com/Startup.cs
@@ -46,19 +46,10 @@ namespace ifesenko.com
         {
             services.AddApplicationInsightsTelemetry(Configuration);
 
-
-            services.AddOptions();
-            services.ConfigureOptions<AppSettings>();
-            services.ConfigureOptions<CacheProfileSettings>();
-            services.ConfigureOptions<SitemapSettings>();
-            services.ConfigureOptions<EmailServiceSettings>();
-
-            //services.Configure<AppSettings>(Configuration.GetSection(nameof(AppSettings)));
-            //services.Configure<CacheProfileSettings>(Configuration.GetSection(nameof(CacheProfileSettings)));
-            //services.Configure<SitemapSettings>(Configuration.GetSection(nameof(SitemapSettings)));
-            //services.Configure<EmailServiceSettings>(Configuration.GetSection(nameof(EmailServiceSettings)));
-
-            
+            services.Configure<AppSettings>(Configuration.GetSection(nameof(AppSettings)));
+            services.Configure<CacheProfileSettings>(Configuration.GetSection(nameof(CacheProfileSettings)));
+            services.Configure<SitemapSettings>(Configuration.GetSection(nameof(SitemapSettings)));
+            services.Configure<EmailServiceSettings>(Configuration.GetSection(nameof(EmailServiceSettings)));
 
             //services.AddCaching();
             // services.AddTransient<IDistributedCache, RedisCache>();
