26bb8edf094ec1e5b79455bbf723b6afe35542a8
Igor Fesenko <igor.aka.ky7m@gmail.com>
2016-07-19 23:05:56 +0300

    Enables WebMarkupMin

--- src/PersonalWebApp/Startup.cs
+++ src/PersonalWebApp/Startup.cs
@@ -13,7 +13,7 @@ using PersonalWebApp.Infrastructure.Services.Implementation.CloudStorageService;
 using PersonalWebApp.Infrastructure.Services.Implementation.HealthService;
 using PersonalWebApp.Infrastructure.Services.Interfaces;
 using PersonalWebApp.Infrastructure.Settings;
-//using WebMarkupMin.AspNetCore1;
+using WebMarkupMin.AspNetCore1;
 
 namespace PersonalWebApp
 {
@@ -55,21 +55,21 @@ namespace PersonalWebApp
                   routeOptions.LowercaseUrls = true;
               });
 
-            // Add WebMarkupMin services to the services container.
-            //services.AddWebMarkupMin(options =>
-            //    {
-            //        options.AllowMinificationInDevelopmentEnvironment = false;
-            //        options.AllowCompressionInDevelopmentEnvironment = false;
-            //        options.DisablePoweredByHttpHeaders = true;
-            //    })
-            //    .AddHtmlMinification(options =>
-            //    {
-            //        var settings = options.MinificationSettings;
-            //        settings.RemoveRedundantAttributes = true;
-            //        settings.RemoveHttpProtocolFromAttributes = true;
-            //        settings.RemoveHttpsProtocolFromAttributes = true;
-            //    })
-            //    .AddHttpCompression();
+           // Add WebMarkupMin services to the services container.
+           services.AddWebMarkupMin(options =>
+               {
+                   //options.AllowMinificationInDevelopmentEnvironment = true;
+                   //options.AllowCompressionInDevelopmentEnvironment = true;
+                   options.DisablePoweredByHttpHeaders = true;
+               })
+               .AddHtmlMinification(options =>
+               {
+                   var settings = options.MinificationSettings;
+                   settings.RemoveRedundantAttributes = true;
+                   settings.RemoveHttpProtocolFromAttributes = true;
+                   settings.RemoveHttpsProtocolFromAttributes = true;
+               })
+               .AddHttpCompression();
 
             services.AddMvc(options =>
             {
@@ -182,7 +182,7 @@ namespace PersonalWebApp
 
             app.UseStaticFiles();
 
-            //app.UseWebMarkupMin();
+            app.UseWebMarkupMin();
 
             app.UseHeadersMiddleware(new HeadersBuilder()
                 .RemoveHeader("Server"));
