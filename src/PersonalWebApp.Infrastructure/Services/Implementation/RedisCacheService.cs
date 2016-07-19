146f513f50ed890fec208c5b553b88e16e92ebb1
Igor Fesenko <igor.aka.ky7m@gmail.com>
2016-07-19 22:58:50 +0300

    Moves from JSON.NET to Wire for serializing.

--- src/PersonalWebApp.Infrastructure/Services/Implementation/RedisCacheService.cs
+++ src/PersonalWebApp.Infrastructure/Services/Implementation/RedisCacheService.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
 using PersonalWebApp.Infrastructure.Services.Interfaces;
 using PersonalWebApp.Infrastructure.Settings;
 using StackExchange.Redis;
+using Wire;
 
 namespace PersonalWebApp.Infrastructure.Services.Implementation
 {
@@ -12,6 +13,8 @@ namespace PersonalWebApp.Infrastructure.Services.Implementation
     {
         private readonly AppSettings _appSettings;
         private readonly Lazy<ConnectionMultiplexer> _cacheDatabase;
+        private readonly Serializer _serializer = new Serializer(new SerializerOptions(true));
+
         public RedisCacheService(IOptions<AppSettings> appSettings)
         {
             _appSettings = appSettings.Value;
@@ -20,11 +23,7 @@ namespace PersonalWebApp.Infrastructure.Services.Implementation
 
         public async Task<bool> StoreAsync<T>(string key, T value, TimeSpan? expiry = null) where T: class
         {
-            string serializedValue = null;
-            if (value != null)
-            {
-                serializedValue = JsonConvert.SerializeObject(value, Formatting.None);
-            }
+            var serializedValue = Serialize(value);
             return await _cacheDatabase.Value.GetDatabase().StringSetAsync(key, serializedValue, expiry);
         }
 
@@ -32,15 +31,47 @@ namespace PersonalWebApp.Infrastructure.Services.Implementation
         {
             var database = _cacheDatabase.Value.GetDatabase();
             var serializedValue = await database.StringGetAsync(key);
-            if (string.IsNullOrEmpty(serializedValue))
-            {
-                return default(T);
-            }
-            return JsonConvert.DeserializeObject<T>(serializedValue);
+            return string.IsNullOrEmpty(serializedValue) ? default(T) : Deserialize<T>(serializedValue);
         }
         public async Task<bool> DeleteAsync(string key)
         {
             return await _cacheDatabase.Value.GetDatabase().KeyDeleteAsync(key);
         }
+
+        #region Private methods
+
+        private byte[] Serialize<T>(T value) where T:class
+        {
+            byte[] objectDataAsStream;
+            if (value == null)
+            {
+                return null;
+            }
+            using (var memoryStream = new MemoryStream())
+            {
+                _serializer.Serialize(value, memoryStream);
+                objectDataAsStream = memoryStream.ToArray();
+            }
+            return objectDataAsStream;
+        }
+
+        private T Deserialize<T>(byte[] sourceBytes)
+        {
+            var result = default(T);
+            if (sourceBytes == null)
+            {
+                return result;
+            }
+            using (var memoryStream = new MemoryStream())
+            {
+                memoryStream.Write(sourceBytes, 0, sourceBytes.Length);
+                memoryStream.Position = 0;
+                result = _serializer.Deserialize<T>(memoryStream);
+            }
+
+            return result;
+        }
+
+        #endregion
     }
 }
