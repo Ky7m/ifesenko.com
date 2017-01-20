d19ad6420ab8c9f4ed124308fe390d03aaff4445
Igor Fesenko <ifesen@softserveinc.com>
2017-01-20 17:34:28 +0000

    Add CachedWebRootFileProvider

---
+++ src/PersonalWebApp/Middleware/AppStart.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace PersonalWebApp.Middleware
+{
+    public class AppStart : IStartupFilter
+    {
+        private readonly IApplicationLifetime _appLifetime;
+        private readonly CachedWebRootFileProvider _cachedWebRoot;
+        private readonly TelemetryClient _telemetry;
+
+        public AppStart(IApplicationLifetime appLifetime, TelemetryClient telemetry, CachedWebRootFileProvider cachedWebRoot)
+        {
+            _appLifetime = appLifetime;
+            _telemetry = telemetry;
+            _cachedWebRoot = cachedWebRoot;
+        }
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) => app =>
+        {
+            // Enable tracking of application start/stop to Application Insights
+            if (_telemetry.IsEnabled())
+            {
+                _appLifetime.ApplicationStarted.Register(() =>
+                {
+                    var startedEvent = new EventTelemetry("Application Started");
+                    _telemetry.TrackEvent(startedEvent);
+                });
+                _appLifetime.ApplicationStopping.Register(() =>
+                {
+                    var startedEvent = new EventTelemetry("Application Stopping");
+                    _telemetry.TrackEvent(startedEvent);
+                });
+                _appLifetime.ApplicationStopped.Register(() =>
+                {
+                    var stoppedEvent = new EventTelemetry("Application Stopped");
+                    _telemetry.TrackEvent(stoppedEvent);
+                    _telemetry.Flush();
+
+                    // Allow some time for flushing before shutdown.
+                    Thread.Sleep(1000);
+                });
+            }
+
+            // Call next now so that the ILoggerFactory by Startup.Configure is configured before we go any further
+            next(app);
+
+            // Prime the cached web root file provider for static file serving
+            _cachedWebRoot.PrimeCache();
+        };
+    }
+}
