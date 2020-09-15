ff20658a219c26d528eb8265bb8ecb0ff2e86182
Igor Fesenko <ifesen@softserveinc.com>
2020-09-14 19:59:24 -0700

    add link to recorded event

--- src/PersonalWebApp/EventsList/Events2020.cs
+++ src/PersonalWebApp/EventsList/Events2020.cs
@@ -51,7 +51,8 @@ namespace PersonalWebApp.EventsList
                         new Dictionary<string, string>
                         {
                             ["https://1drv.ms/p/s!AmdJq1kgIxHUkM0VMMrKi-6tYt3TZQ?e=x7lTKu"] = CommonStrings.Powerpoint,
-                            ["https://github.com/Ky7m/DemoCode/tree/master/ApplicationDiagnosticsNetCore"] = CommonStrings.DemoCode
+                            ["https://github.com/Ky7m/DemoCode/tree/master/ApplicationDiagnosticsNetCore"] = CommonStrings.DemoCode,
+                            ["https://www.facebook.com/149447525111721/videos/180335746489420/"] = CommonStrings.Webcast
                         })
                 },
                 Location = CommonStrings.Webcast,
