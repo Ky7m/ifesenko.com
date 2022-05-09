c23f00fd9c0bbd276b5321e1a261c09dcff8d280
Igor Fesenko <ky7m@hotmail.com>
2022-05-09 09:49:49 -0700

    add a new event

--- src/PersonalWebApp/EventsList/Events2022.cs
+++ src/PersonalWebApp/EventsList/Events2022.cs
@@ -11,6 +11,23 @@ internal static class Events2022
     {
         new()
         {
+            Title = "Svitla Smart Talk",
+            Link = "https://kommunity.com/svitla-systems/events/svitla-smart-talk-top-things-we-do-wrong-with-telemetry-data-in-net-e5430dd0",
+            Items = new []
+            {
+                new EventModelItem("Top Things We Do Wrong With Telemetry Data In .NET",
+                    new Dictionary<string, string>
+                    {
+                        ["https://1drv.ms/p/s!AmdJq1kgIxHUkO5q4HoH_2zdjS75dA?e=tIh2bx"] = CommonStrings.Powerpoint,
+                        ["https://github.com/Ky7m/DemoCode/tree/main/WorkWithTelemetryInDotNET"] = CommonStrings.DemoCode
+                        //["https://www.youtube.com/watch?v=PY6RshfGT34"] = CommonStrings.Recording
+                    })
+            },
+            Location = CommonStrings.Online,
+            Date = new DateTime(2022, 5, 26)
+        },
+        new()
+        {
             Title = "MS Stage Conference",
             Link = "https://msstage.com/speakers/igor-fesenko/",
             Items = new []
