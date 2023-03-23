de12ce593f7320a59fd41fcdbc4e52c856a6029c
Igor Fesenko <ky7m@hotmail.com>
2023-03-22 23:13:52 -0400

    fix link

--- src/PersonalWebApp/EventsList/Events2022.cs
+++ src/PersonalWebApp/EventsList/Events2022.cs
@@ -33,7 +33,7 @@ internal static class Events2022
                 new EventModelItem("Top Things We Do Wrong With Telemetry Data In .NET",
                     new Dictionary<string, string>
                     {
-                        ["https://1drv.ms/p/s!AmdJq1kgIxHUkO5q4HoH_2zdjS75dA?e=tIh2bx"] = CommonStrings.Powerpoint,
+                        ["https://1drv.ms/p/s!AmdJq1kgIxHUkO5qNdva279NMxLG1w?e=qc0UuN"] = CommonStrings.Powerpoint,
                         ["https://github.com/Ky7m/DemoCode/tree/main/WorkWithTelemetryInDotNET"] = CommonStrings.DemoCode,
                         ["https://www.youtube.com/watch?v=LdYBE8ftw18"] = CommonStrings.Recording
                     })
@@ -50,7 +50,7 @@ internal static class Events2022
                 new EventModelItem("Top Things We Do Wrong With Telemetry Data In .NET",
                     new Dictionary<string, string>
                     {
-                        ["https://1drv.ms/p/s!AmdJq1kgIxHUkO5q4HoH_2zdjS75dA?e=tIh2bx"] = CommonStrings.Powerpoint,
+                        ["https://1drv.ms/p/s!AmdJq1kgIxHUkO5qNdva279NMxLG1w?e=qc0UuN"] = CommonStrings.Powerpoint,
                         ["https://github.com/Ky7m/DemoCode/tree/main/WorkWithTelemetryInDotNET"] = CommonStrings.DemoCode,
                         ["https://www.youtube.com/watch?v=ICy2m5tL730"] = CommonStrings.Recording
                     })
